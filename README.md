6d889e4526c8eb91dbf850d0f6e9b123d35292b4
LanChi Pham <lpham2525@gmail.com>
2020-05-14 21:13:29 -0700

    added README.gif

--- README.md
+++ README.md
@@ -1,32 +1,31 @@
 
 
   [![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)](http://commonmark.org)
-  ----------------------------------------------------------------------------------
+  --------------------------------------------------------------
 # Title: README
 ----------------------------------------------------------------
-###  Description: Generates READMEs with basic info about a project.
+###  Description: Generates READMEs with basic information about a project.
 ----------------------------------------------------------------
-##Demo: [![readme-gif]./README.gif]]
-
-##Table of Contents
+#Demo: ![readme-gif](README.gif)
+-------------------------------------------------------------
+#Table of Contents
 ----------------------------------------------------------------
 
  *Installation: npm i
- ----------------------------------------------------------
 
  *Usage:The generated README will have the user's profile picture and email on it.
------------------------------------------------------------
- *License: None
-------------------------------------------------------------
- *Contributing: Contributions welcome.
-------------------------------------------------------------
- *Test: npm test
---------------------------------------------------------
-###Questions? 
--------------------------------------------------
-*[![Profile-picture](https://avatars2.githubusercontent.com/u/60991372?v=4)]]
-----------------------------------------------------
-###Email: null
 
-###Made by LanChi Pham, &copy;Copyright 2020
+ *License: None
+
+ *Contributing: Contributions welcome.
+
+ *Test: npm test
+
+#Questions? 
+
+* ![Profile-picture](https://avatars2.githubusercontent.com/u/60991372?v=4)
+
+#Email: null
+
+#Made by LanChi Pham, &copy;Copyright 2020
   
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,34 +2,33 @@ function generateMarkdown(inquirerResponses, data) {
   return `
 
   [![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)](http://commonmark.org)
-  ----------------------------------------------------------------------------------
+  --------------------------------------------------------------
 # Title: ${ inquirerResponses.title}
 ----------------------------------------------------------------
 ###  Description: ${ inquirerResponses.description}
 ----------------------------------------------------------------
-##Demo: [![readme-gif]./README.gif]]
-
-##Table of Contents
+#Demo: ![readme-gif](README.gif)
+-------------------------------------------------------------
+#Table of Contents
 ----------------------------------------------------------------
 
  *Installation: ${ inquirerResponses.installation}
- ----------------------------------------------------------
 
  *Usage:${ inquirerResponses.usage}
------------------------------------------------------------
- *License: ${ inquirerResponses.license}
-------------------------------------------------------------
- *Contributing: ${ inquirerResponses.contributing}
-------------------------------------------------------------
- *Test: ${ inquirerResponses.test}
---------------------------------------------------------
-###Questions? 
--------------------------------------------------
-*[![Profile-picture](${data.avatar_url})]]
-----------------------------------------------------
-###Email: ${data.email}
 
-###Made by LanChi Pham, &copy;Copyright 2020
+ *License: ${ inquirerResponses.license}
+
+ *Contributing: ${ inquirerResponses.contributing}
+
+ *Test: ${ inquirerResponses.test}
+
+#Questions? 
+
+* ![Profile-picture](${data.avatar_url})
+
+#Email: ${data.email}
+
+#Made by LanChi Pham, &copy;Copyright 2020
   `
 }
 
